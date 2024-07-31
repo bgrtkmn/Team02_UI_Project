708d3cfb4e850bbf7fe6bd943d65388f9cd4fb08
Buğra <167450228+bgrtkmn@users.noreply.github.com>
2024-07-31 16:31:16 +0300

    Update OnlineMasterMarket.java

--- src/test/java/pages/OnlineMasterMarket.java
+++ src/test/java/pages/OnlineMasterMarket.java
@@ -7,7 +7,7 @@ import utilities.Driver;
 
 public class OnlineMasterMarket {
 
-    public OnlineMasterMarket{
+    public OnlineMasterMarket(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
