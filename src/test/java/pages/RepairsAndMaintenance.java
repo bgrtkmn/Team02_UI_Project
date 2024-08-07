e94a262bcfdc3321d870f7f923414fa44495e4bf
rasit <mrasitcl@gmail.com>
2024-08-07 10:18:56 +0300

    .

--- src/test/java/pages/RepairsAndMaintenance.java
+++ src/test/java/pages/RepairsAndMaintenance.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
-public class RepairsAndMaintenance {
+public class  RepairsAndMaintenance {
 
     public RepairsAndMaintenance() {
         PageFactory.initElements(Driver.getDriver(),this);
