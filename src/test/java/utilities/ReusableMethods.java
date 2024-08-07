32c8e7affaef4a5984153a1d67ae21aa5b39d533
rasit <mrasitcl@gmail.com>
2024-08-07 10:57:48 +0300

    070824

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -19,7 +19,7 @@ import java.util.Set;
 
 public class ReusableMethods {
 
-    public static void bekle(int saniye){
+    public static void bekle(int saniye) {
 
         try {
             Thread.sleep(saniye * 1000);
@@ -29,12 +29,12 @@ public class ReusableMethods {
 
     }
 
-    public static List<String> stringListeDonustur(List<WebElement> webElementList){
+    public static List<String> stringListeDonustur(List<WebElement> webElementList) {
 
         List<String> donusturulenList = new ArrayList<>();
 
         for (WebElement eachElement : webElementList
-             ) {
+        ) {
 
             donusturulenList.add(eachElement.getText());
         }
@@ -43,7 +43,7 @@ public class ReusableMethods {
         return donusturulenList;
     }
 
-    public static void switchWindow(String hedefUrl){
+    public static void switchWindow(String hedefUrl) {
 
         // birden fazla window acik iken bu method calisacak
 
@@ -56,11 +56,11 @@ public class ReusableMethods {
         //    loop'u bitirelim
 
         for (String eachWHD : tumWHDSeti
-             ) {
+        ) {
 
             Driver.getDriver().switchTo().window(eachWHD);
 
-            if (Driver.getDriver().getCurrentUrl().equals(hedefUrl)){
+            if (Driver.getDriver().getCurrentUrl().equals(hedefUrl)) {
                 break;
             }
 
@@ -68,7 +68,7 @@ public class ReusableMethods {
 
     }
 
-    public static void tumSayfaScreenshot(WebDriver driver){
+    public static void tumSayfaScreenshot(WebDriver driver) {
 
         // 1- takesScreenshot objesi olusturalim
         TakesScreenshot tss = (TakesScreenshot) driver;
@@ -84,7 +84,7 @@ public class ReusableMethods {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("YYMMddHHmmss");
         String tarihDamgasi = ldt.format(dtf); // 240402123223
 
-        kaydedilecekDosyaYolu = "target/screenshots/tumSayfaSS"+tarihDamgasi+".jpeg";
+        kaydedilecekDosyaYolu = "target/screenshots/tumSayfaSS" + tarihDamgasi + ".jpeg";
 
         File tumSayfaScreenshot = new File(kaydedilecekDosyaYolu);
 
@@ -95,14 +95,14 @@ public class ReusableMethods {
         // 4- gecici dosyayi, asil kaydetmek istedigimiz dosyaya kopyalayalim
 
         try {
-            org.apache.commons.io.FileUtils.copyFile(geciciDosya,tumSayfaScreenshot);
+            org.apache.commons.io.FileUtils.copyFile(geciciDosya, tumSayfaScreenshot);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
     }
 
-    public static void tumSayfaScreenshot(WebDriver driver,String raporIsmi){
+    public static void tumSayfaScreenshot(WebDriver driver, String raporIsmi) {
 
         // 1- takesScreenshot objesi olusturalim
         TakesScreenshot tss = (TakesScreenshot) driver;
@@ -118,7 +118,7 @@ public class ReusableMethods {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("YYMMddHHmmss");
         String tarihDamgasi = ldt.format(dtf); // 240402123223
 
-        kaydedilecekDosyaYolu = "target/screenshots/"+raporIsmi+tarihDamgasi+".jpeg";
+        kaydedilecekDosyaYolu = "target/screenshots/" + raporIsmi + tarihDamgasi + ".jpeg";
 
         File tumSayfaScreenshot = new File(kaydedilecekDosyaYolu);
 
@@ -129,20 +129,20 @@ public class ReusableMethods {
         // 4- gecici dosyayi, asil kaydetmek istedigimiz dosyaya kopyalayalim
 
         try {
-            org.apache.commons.io.FileUtils.copyFile(geciciDosya,tumSayfaScreenshot);
+            org.apache.commons.io.FileUtils.copyFile(geciciDosya, tumSayfaScreenshot);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
     }
 
-    public static void webelementScreenshot(WebElement istenenElement){
+    public static void webelementScreenshot(WebElement istenenElement) {
 
         LocalDateTime ldt = LocalDateTime.now();
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("YYMMddHHmmss");
         String tarihDamgasi = ldt.format(dtf); // 240402123223
 
-        String kaydedilecekDosyaYolu = "target/screenshots/webelementSS"+tarihDamgasi+".jpeg";
+        String kaydedilecekDosyaYolu = "target/screenshots/webelementSS" + tarihDamgasi + ".jpeg";
 
         // 1- screenshot alinacak webelemnti locate edip kaydedelim
 
@@ -157,18 +157,18 @@ public class ReusableMethods {
         // 4- gecici dosyayi ana dosyaya kopyalayalim
 
         try {
-            org.apache.commons.io.FileUtils.copyFile(geciciDosya,webelementSS);
+            org.apache.commons.io.FileUtils.copyFile(geciciDosya, webelementSS);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static void webelementScreenshot(WebElement istenenElement,String raporIsmi){
+    public static void webelementScreenshot(WebElement istenenElement, String raporIsmi) {
         LocalDateTime ldt = LocalDateTime.now();
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("YYMMddHHmmss");
         String tarihDamgasi = ldt.format(dtf); // 240402123223
 
-        String kaydedilecekDosyaYolu = "target/screenshots/"+raporIsmi+tarihDamgasi+".jpeg";
+        String kaydedilecekDosyaYolu = "target/screenshots/" + raporIsmi + tarihDamgasi + ".jpeg";
 
         // 1- screenshot alinacak webelemnti locate edip kaydedelim
 
@@ -183,7 +183,7 @@ public class ReusableMethods {
         // 4- gecici dosyayi ana dosyaya kopyalayalim
 
         try {
-            org.apache.commons.io.FileUtils.copyFile(geciciDosya,webelementSS);
+            org.apache.commons.io.FileUtils.copyFile(geciciDosya, webelementSS);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -203,6 +203,19 @@ public class ReusableMethods {
         return target;
     }
 
+    public static void typeKeys(String text, WebElement element) {
+        element.clear();
 
-
+        for (String c : text.split("")) {
+            element.sendKeys(c);
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
+
+
+
